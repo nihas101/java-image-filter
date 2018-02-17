c12a7f26c306839cf96cf3ef7259065e557e0381
Nikita Hasert <nikitahasert@gmail.com>
2018-02-17 17:44:12 +0100

    Inherit from MosaicFilter

--- src/main/java/de/nihas101/pictureFilter/filters/filtersNxN/SNESMosaicFilter.java
+++ src/main/java/de/nihas101/pictureFilter/filters/filtersNxN/SNESMosaicFilter.java
@@ -1,11 +1,10 @@
 package de.nihas101.pictureFilter.filters.filtersNxN;
 
-import de.nihas101.pictureFilter.filters.Filter;
 import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 
-public class SNESMosaicFilter extends PixelIterator5x5 implements Filter {
+public class SNESMosaicFilter extends MosaicFilter {
     @Override
     public void applyFilter(Image image, WritableImage writableImage) {
         setup(image, writableImage);
@@ -14,11 +13,7 @@ public class SNESMosaicFilter extends PixelIterator5x5 implements Filter {
                 (x, y) ->{
                     /* Get top-left color */
                     Color color = pixelReader.getColor(x[0],y[0]);
-
-                    /* Apply color to whole square */
-                    for(int i=1 ; i < x.length ; i++) {
-                        pixelWriter.setColor(x[i], y[i], color);
-                    }
+                    applyToSquare(x, y, color);
                 });
     }
 
